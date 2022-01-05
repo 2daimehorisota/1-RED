a0d6161048a315b5b527363ae087fdea61c1c263
2daime <onlygithubsota@gmail.com>
2022-01-05 02:21:14 +0000

    log

--- myled/myled.c
+++ myled/myled.c
@@ -2,11 +2,13 @@
 
 static int __init init_mod(void) //カーネルモジュールの初期化
 {
+	printk(KERN_INFO "%s is loaded.\n",__FILE__);
 	        return 0;
 }
 
 static void __exit cleanup_mod(void) //後始末
 {
+	printk(KERN_INFO "%s is unloaded.\n",__FILE__);
 }
 
 module_init(init_mod);     // マクロで関数を登録
